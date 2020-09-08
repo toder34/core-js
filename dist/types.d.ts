toder34/core-js

commit c3a79daf1dddf5f2262b35c9e84b072e92050334

Include netAddress in plain Client.AddressInfo (#555)

dist/types.d.ts
@@ -153,11 +153,16 @@ declare class ClientBasicAddress {
     public peerAddress: PeerAddress;
     public peerId: PeerId;
     public services: string[];
+    public netAddress: NetAddress | null;
     constructor(address: PeerAddress);
     public toPlain(): {
         peerAddress: string,
         peerId: string,
         services: string[],
+        netAddress: {
+            ip: Uint8Array,
+            reliable: boolean,
+        } | null,
     };
 }
 
@@ -3402,7 +3407,7 @@ export class PeerAddress {
     public protocol: number;
     public services: number;
     public timestamp: number;
-    public netAddress: NetAddress;
+    public netAddress: NetAddress | null;
     public publicKey: PublicKey;
     public peerId: PeerId;
     public distance: number;

src/main/generic/api/NetworkClient.js
@@ -165,12 +165,21 @@ Client.BasicAddress = class BasicAddress {
         return Services.toNameArray(Services.legacyProvideToCurrent(this._address.services));
     }
 
+    /** @type {NetAddress} */
+    get netAddress() {
+        return this._address.netAddress;
+    }
+
     /** @type {object} */
     toPlain() {
         return {
             peerAddress: this.peerAddress.toString(),
             peerId: this.peerId.toString(),
-            services: this.services
+            services: this.services,
+            netAddress: this.netAddress ? {
+                ip: this.netAddress.ip,
+                reliable: this.netAddress.reliable,
+            } : null,
         };
     }
 };

commit 7602adfa4775ed4bab5a78c5b6a58e7464f8bebf

Fix class inheritance error introduced in #555

dist/types.d.ts
@@ -153,7 +153,7 @@ declare class ClientBasicAddress {
     public peerAddress: PeerAddress;
     public peerId: PeerId;
     public services: string[];
-    public netAddress: NetAddress | null;
+    public netAddress: NetAddress;
     constructor(address: PeerAddress);
     public toPlain(): {
         peerAddress: string,
@@ -175,6 +175,10 @@ declare class ClientAddressInfo extends ClientBasicAddress {
         peerAddress: string,
         peerId: string,
         services: string[],
+        netAddress: {
+            ip: Uint8Array,
+            reliable: boolean,
+        } | null,
         banned: boolean,
         connected: boolean,
     };
@@ -182,7 +186,6 @@ declare class ClientAddressInfo extends ClientBasicAddress {
 
 declare class ClientPeerInfo extends ClientBasicAddress {
     public connectionSince: number;
-    public netAddress: NetAddress;
     public bytesReceived: number;
     public bytesSent: number;
     public latency: number;
@@ -196,8 +199,11 @@ declare class ClientPeerInfo extends ClientBasicAddress {
         peerAddress: string,
         peerId: string,
         services: string[],
+        netAddress: {
+            ip: Uint8Array,
+            reliable: boolean,
+        } | null,
         connectionSince: number,
-        netAddress: string,
         bytesReceived: number,
         bytesSent: number,
         latency: number,

src/main/generic/api/NetworkClient.js
@@ -176,7 +176,7 @@ Client.BasicAddress = class BasicAddress {
             peerAddress: this.peerAddress.toString(),
             peerId: this.peerId.toString(),
             services: this.services,
-            netAddress: this.netAddress ? {
+            netAddress: this.netAddress.ip ? {
                 ip: this.netAddress.ip,
                 reliable: this.netAddress.reliable,
             } : null,
@@ -292,7 +292,6 @@ Client.PeerInfo = class PeerInfo extends Client.BasicAddress {
     toPlain() {
         const plain = super.toPlain();
         plain.connectionSince = this.connectionSince;
-        plain.netAddress = this.netAddress.toString();
         plain.bytesReceived = this.bytesReceived;
         plain.bytesSent = this.bytesSent;
         plain.latency = this.latency;